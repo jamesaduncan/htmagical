d3d989551c10cddfc0bd842ff7a4a5b37310c355
James A. Duncan <james@kinburnstreet.consulting>
2025-01-15 14:41:07 +0000

    working in chrome now too

--- static/js/HTMagical.js
+++ static/js/HTMagical.js
@@ -1,146 +1,149 @@
+console.log("running HTMagical.js");
+
 /* htmagical buttons */
-if ( document ) {
-    document.addEventListener("DOMContentLoaded", async () => {
-        HTMLElement.prototype.registerSelector  = function( selector, callback ) {
-            if (!this._selectorReg) this._selectorReg = {};
-            const registry = this._selectorReg;
-            if ( registry[ selector ] ) {
-                registry[ selector ].push( callback );
-            } else {
-                registry[ selector ] = [ callback ];
-            }
-        };
-        document.registerSelector = HTMLElement.prototype.registerSelector;
+document._selectorReg = {};
 
-        /* this sets up the mutation observer to watch the document, and
-            execute on any callbacks whenever we see registered selectors appear */
-        self.addEventListener("load", () => {
-            const registry = document._selectorReg;    
-            const selectors = Object.keys( document._selectorReg );
-            for ( const selector of selectors ) {
-                const nodes = document.querySelectorAll( selector );
-                for ( const node of nodes ) {
-                    for ( const cb of registry[ selector ] ) {
-                        cb( node, selector );
-                    }      
-                }    
-            }
-            
-            const observer = new MutationObserver( function( mutationList, observer ) {
-                for ( const mutation of mutationList ) {
-                    if ( mutation.addedNodes.length > 0 ) {
-                        mutation.addedNodes.forEach( (node) => {
-                            for ( const selector of selectors ) {
-                                if ( !node.querySelectorAll ) break;
-                                const nodes = node.querySelectorAll( selector );                        
-                                for ( const cbnode of nodes ) {
-                                    for ( const cb of registry[ selector ] ) {
-                                        cb( cbnode, selector );
-                                    }      
-                                }	     
-                            }
-                        });
-                    }
-                }
-            });
-            
-            observer.observe( document.querySelector('body'), { childList: true, subtree: true });
-        });
-        
+document.addEventListener("DOMContentLoaded", async () => {
+    console.log("in the DOMContentLoaded event");
 
-        class HTMagical {
-            static for ( aThing ) {
-                const selector = aThing.getAttribute('for');
-                const part = document.querySelector( selector );
-                return part;
-            }
+    HTMLElement.prototype.registerSelector  = function( selector, callback ) {
+//            if (!this._selectorReg) this._selectorReg = {};
+        const registry = this._selectorReg;
+        if ( registry[ selector ] ) {
+            registry[ selector ].push( callback );
+        } else {
+            registry[ selector ] = [ callback ];
+        }
+    };
+    document.registerSelector = HTMLElement.prototype.registerSelector;
 
-            static generateSelector( el ) {
-                let path = [], parent;
-                while (parent = el.parentNode) {
-                path.unshift(`${el.tagName}:nth-child(${[].indexOf.call(parent.children, el)+1})`);
-                el = parent;
-                }
-                return `${path.join(' > ')}`.toLowerCase();        
-            }
-
-            static action( aThing ) {
-                if ( aThing.hasAttribute('action') ) {
-                    const selector = aThing.getAttribute('action');
-
-                    const match = selector.match(/^\:parent\((\-{0,1}\d+)\)/);
-                    if ( match ) {
-                        let node = aThing;
-                        let count = parseInt(match[1]);
-                        while(count < 0) {
-                            node = node.parentNode;
-                            count++;                    
-                        }
-                        return HTMagical.generateSelector( node )
-                    } else {
-                        return selector;
-                    }
-
-                }
-                return HTMagical.generateSelector( aThing );
-            }
-
-            static url( aThing ) {
-                return window.location;
-            }
-
-            static method( aThing ) {
-                return aThing.getAttribute("method").toLowerCase() || 'get';
-            }
-
-            static enctype( aThing ) {
-                return aThing.getAttribute('enctype') || 'text/html';
-            }
-
-            static headers( aThing ) {
-                const headers = {
-                    'Content-Type': HTMagical.enctype( aThing )
-                };
-                const method = this.method( aThing );
-                if ( method == "patch" || method == "delete" ) {
-                    headers['Range'] = `selector=${HTMagical.action( aThing )}`; 
-                }
-                return new Headers( headers );
-            }
+    class HTMagical {
+        static for ( aThing ) {
+            const selector = aThing.getAttribute('for');
+            const part = document.querySelector( selector );
+            return part;
         }
 
-        /* make sure we always update the value element when input values change */
-        document.querySelectorAll('input').forEach( (input) => {
-            input.addEventListener('change', (event) => {
-                event.target.setAttribute('value', event.target.value)
+        static generateSelector( el ) {
+            let path = [], parent;
+            while (parent = el.parentNode) {
+            path.unshift(`${el.tagName}:nth-child(${[].indexOf.call(parent.children, el)+1})`);
+            el = parent;
+            }
+            return `${path.join(' > ')}`.toLowerCase();        
+        }
+
+        static action( aThing ) {
+            if ( aThing.hasAttribute('action') ) {
+                const selector = aThing.getAttribute('action');
+
+                const match = selector.match(/^\:parent\((\-{0,1}\d+)\)/);
+                if ( match ) {
+                    let node = aThing;
+                    let count = parseInt(match[1]);
+                    while(count < 0) {
+                        node = node.parentNode;
+                        count++;                    
+                    }
+                    return HTMagical.generateSelector( node )
+                } else {
+                    return selector;
+                }
+
+            }
+            return HTMagical.generateSelector( aThing );
+        }
+
+        static url( aThing ) {
+            return window.location;
+        }
+
+        static method( aThing ) {
+            return aThing.getAttribute("method").toUpperCase() || 'GET';
+        }
+
+        static enctype( aThing ) {
+            return aThing.getAttribute('enctype') || 'text/html';
+        }
+
+        static headers( aThing ) {
+            const headers = {
+                'Content-Type': HTMagical.enctype( aThing )
+            };
+            const method = this.method( aThing );
+            if ( method == "PATCH" || method == "DELETE" ) {
+                headers['Range'] = `selector=${HTMagical.action( aThing )}`; 
+            }
+            return new Headers( headers );
+        }
+    }
+
+    /* make sure we always update the value element when input values change */
+    document.querySelectorAll('input').forEach( (input) => {
+        input.addEventListener('change', (event) => {
+            event.target.setAttribute('value', event.target.value)
+        });
+    })
+
+    const actionHandler = await import("/js/Action.js");
+
+    document.registerSelector( 'button[method]', (button) => {
+        console.log("processing selector event for button", button);
+        button.addEventListener('click', async (event) => {
+            const part    = HTMagical.for( button );
+            const url     = HTMagical.url( button );
+            const method  = HTMagical.method( button );
+            const headers = HTMagical.headers( button );
+            const response = await fetch(url, {
+                method: method,
+                body: part.outerHTML,
+                headers: headers
             });
-        })
-
-        const actionHandler = await import("/js/Action.js");
-
-        document.registerSelector( 'button[method]', (button) => {
-            button.addEventListener('click', async (event) => {
-                const part    = HTMagical.for( button );
-                const url     = HTMagical.url( button );
-                const method  = HTMagical.method( button );
-                const headers = HTMagical.headers( button );
-                const response = await fetch(url, {
-                    method: method,
-                    body: part.outerHTML,
-                    headers: headers
+            if ( response.ok ) {
+                const selector = HTMagical.action( button );
+                const clone    = part.cloneNode( true );            
+                await actionHandler.default( document, {
+                    selected: document.querySelector( selector ),
+                    root: clone,
+                    method : method.toUpperCase(),
+                    headers: headers, 
+                    selector: selector
                 });
-                if ( response.ok ) {
-                    const selector = HTMagical.action( button );
-                    const clone    = part.cloneNode( true );            
-                    await actionHandler.default( document, {
-                        selected: document.querySelector( selector ),
-                        root: clone,
-                        method : method.toUpperCase(),
-                        headers: headers, 
-                        selector: selector
+            }
+        });
+    });
+
+    /* this sets up the mutation observer to watch the document, and
+        execute on any callbacks whenever we see registered selectors appear */
+        const registry = document._selectorReg;    
+        const selectors = Object.keys( document._selectorReg );
+        console.log(`selectors are `, selectors)
+        for ( const selector of selectors ) {
+            const nodes = document.querySelectorAll( selector );
+            for ( const node of nodes ) {
+                for ( const cb of registry[ selector ] ) {
+                    cb( node, selector );
+                }      
+            }    
+        }
+        
+        const observer = new MutationObserver( function( mutationList, observer ) {
+            for ( const mutation of mutationList ) {
+                if ( mutation.addedNodes.length > 0 ) {
+                    mutation.addedNodes.forEach( (node) => {
+                        for ( const selector of selectors ) {
+                            if ( !node.querySelectorAll ) break;
+                            const nodes = node.querySelectorAll( selector );                        
+                            for ( const cbnode of nodes ) {
+                                for ( const cb of registry[ selector ] ) {
+                                    cb( cbnode, selector );
+                                }      
+                            }	     
+                        }
                     });
                 }
-            });
+            }
         });
-    }, false);
-}
+        
+        observer.observe( document.querySelector('body'), { childList: true, subtree: true });
+}, false);
